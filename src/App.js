48d69976973098f67ff3bfbe87ac9d5aaf0822bc
lais <laisgarcia@usp.br>
2021-02-26 20:04:46 -0300

    add logos

--- src/App.js
+++ src/App.js
@@ -25,52 +25,57 @@ export default function App() {
         {data.map((item, i) => (
                 <Fragment key={i}>
                   <div className="item">
-                    <p className="itemNome">{item.Nome}</p>
-                      {item.Sobre && 
-                      <div>
-                        <p className="subtitulo">Sobre a instituição</p>
-                        <p>{item.Sobre}</p>
-                        <hr/>
-                      </div> }
-                      {(item.Contato||item.Facebook||item.Instagram) && 
-                      <div>
-                        <p className="subtitulo">Contato</p>
-                        <p>{item.Contato}</p>
-                        {item.Facebook && 
-                          <a href={item.Facebook} target="_blank" rel="noreferrer">Facebook <br/></a>
-                        }
-                        {item.Instagram && 
-                          <a href={item.Instagram} target="_blank" rel="noreferrer">Instagram</a>
-                        }
-                      </div>}
-                      {(item.NomeConta||item.CPF||item.Banco||item.Agencia||item.Conta) && 
-                      <div className="bancario">
-                      <hr/>
-                        <p className="subtitulo">Dados para doação</p>
-                        {item.NomeConta && <p><b>Nome: </b> {item.NomeConta}</p>}
-                        {item.CPF && <p><b>CPF: </b>{item.CPF}</p>}
-                        {item.Banco && <p><b>Banco: </b>{item.Banco}</p>}
-                        {item.Agencia && <p><b>Agência: </b>{item.Agencia}</p>}
-                        {item.Conta && <p><b>Conta: </b>{item.Conta}</p>}
-                        {(item.NomeConta2||item.CPF2||item.Banco2||item.Agencia2||item.Conta2) && 
-                        <div>
-                          <p>ou</p>
-                          {item.NomeConta2 && <p><b>Nome: </b> {item.NomeConta2}</p>}
-                          {item.CPF2 && <p><b>CPF: </b>{item.CPF2}</p>}
-                          {item.Banco2 && <p><b>Banco: </b>{item.Banco2}</p>}
-                          {item.Agencia2 && <p><b>Agência: </b>{item.Agencia2}</p>}
-                          {item.Conta2 && <p><b>Conta: </b>{item.Conta2}</p>}
-                        </div>}
-                      </div> }
-                      {(item.LinkDoacao||item.Outros) && 
-                      <div>
-                        <hr/>
-                        <p className="subtitulo">Outras formas de doar</p>
-                          {item.LinkDoacao && <a href={item.LinkDoacao} target="_blank" rel="noreferrer">{item.LinkDoacao}</a>}
-                          {item.Outros && <p>{item.Outros}</p>}
-                      </div>
+                    <div class="img-row">
+                      {item.Logo && 
+                        <img className="logo" src={item.Logo} alt="logo"/>
                       }
-                  </div>
+                    </div>
+                      <p className="itemNome">{item.Nome}</p>
+                        {item.Sobre && 
+                        <div>
+                          <p className="subtitulo">Sobre a instituição</p>
+                          <p>{item.Sobre}</p>
+                          <hr/>
+                        </div> }
+                        {(item.Contato||item.Facebook||item.Instagram) && 
+                        <div>
+                          <p className="subtitulo">Contato</p>
+                          <p>{item.Contato}</p>
+                          {item.Facebook && 
+                            <a href={item.Facebook} target="_blank" rel="noreferrer">Facebook <br/></a>
+                          }
+                          {item.Instagram && 
+                            <a href={item.Instagram} target="_blank" rel="noreferrer">Instagram</a>
+                          }
+                        </div>}
+                        {(item.NomeConta||item.CPF||item.Banco||item.Agencia||item.Conta) && 
+                        <div className="bancario">
+                        <hr/>
+                          <p className="subtitulo">Dados para doação</p>
+                          {item.NomeConta && <p><b>Nome: </b> {item.NomeConta}</p>}
+                          {item.CPF && <p><b>CPF: </b>{item.CPF}</p>}
+                          {item.Banco && <p><b>Banco: </b>{item.Banco}</p>}
+                          {item.Agencia && <p><b>Agência: </b>{item.Agencia}</p>}
+                          {item.Conta && <p><b>Conta: </b>{item.Conta}</p>}
+                          {(item.NomeConta2||item.CPF2||item.Banco2||item.Agencia2||item.Conta2) && 
+                          <div>
+                            <p>ou</p>
+                            {item.NomeConta2 && <p><b>Nome: </b> {item.NomeConta2}</p>}
+                            {item.CPF2 && <p><b>CPF: </b>{item.CPF2}</p>}
+                            {item.Banco2 && <p><b>Banco: </b>{item.Banco2}</p>}
+                            {item.Agencia2 && <p><b>Agência: </b>{item.Agencia2}</p>}
+                            {item.Conta2 && <p><b>Conta: </b>{item.Conta2}</p>}
+                          </div>}
+                        </div> }
+                        {(item.LinkDoacao||item.Outros) && 
+                        <div>
+                          <hr/>
+                          <p className="subtitulo">Outras formas de doar</p>
+                            {item.LinkDoacao && <a href={item.LinkDoacao} target="_blank" rel="noreferrer">{item.LinkDoacao}</a>}
+                            {item.Outros && <p>{item.Outros}</p>}
+                        </div>
+                        }
+                    </div>
                 </Fragment>
         ))}
       </Container>
