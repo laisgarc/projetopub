779d4e4e6d9d3f56678ab402ca0d059a663c8427
lais <laisgarcia@usp.br>
2021-03-02 15:04:29 -0300

    css filtros

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,7 @@ import "../index.css";
 const Cards = (props) => {
 
   const dataBackup = props.datas
-  const [array, setArray] = useState([...props.datas]);
+  const [array, setArray] = useState(dataBackup);
 
   const estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA',
   'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR',
